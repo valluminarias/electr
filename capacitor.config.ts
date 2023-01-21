6654cad31db3e527572f99a76144321ab357ad6c
Val Luminarias <vluminarias@yahoo.com>
2023-01-21 12:34:54 +0800

    Update displayname

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.electr',
-  appName: 'electr',
+  appName: 'Electr',
   webDir: 'www',
   bundledWebRuntime: false
 };
